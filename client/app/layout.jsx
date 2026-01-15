901423041e1ee338b7bff0c2691b0682e5dab755
Chandani <nittianchandani@gmail.com>
2026-01-15 17:28:30 +0530

    Adding root layout JSX to my folder

---
+++ client/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "CampusConnect",
+  description: "Connects students ,request and approves easily",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+ 
