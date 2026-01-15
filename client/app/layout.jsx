fe1246e0eb580018d9186c0c49d03a57472a34d2
Chandani <nittianchandani@gmail.com>
2026-01-16 02:25:35 +0530

    Adding register pages and basics style it

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata = {
   title: "CampusConnect",
@@ -7,7 +8,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Navbar */}
+        <header className="navbar">
+          <h6 className="logo">CampusConnect</h6>
+
+          <nav className="nav-links">
+            <Link href="/">Home</Link>
+            <Link href="/">Pages</Link>
+            <Link href="/request">Request</Link>
+            <Link href="/">Dashboard</Link>
+          </nav>
+        </header>
+
+        {/* Page Content */}
+        {/* <main className="page-wrapper"> */}
+          {children}
+          {/* </main> */}
+      </body>
     </html>
   );
 }
---
+++ client/app/register/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.password !== formData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    alert("Registration Successful");
+    console.log(formData);
+  };
+
+  return (
+    <section className="auth-wrapper">
+      <div className="auth-card">
+        <h2>Create Account</h2>
+        <p className="subtitle">
+          Join CampusConnect using your college email
+        </p>
+
+        <form onSubmit={handleSubmit} className="auth-form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            required
+            onChange={handleChange}
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="College Email"
+            required
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            required
+            onChange={handleChange}
+          />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </section>
+  );
+}
