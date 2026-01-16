9a8fe68c5888b1c093112eaf31109152dec62480
Chandani <nittianchandani@gmail.com>
2026-01-16 23:19:02 +0530

    changes in layoutand globals css

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -9,22 +9,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Navbar */}
         <header className="navbar">
-          <h6 className="logo">CampusConnect</h6>
+          <h5 className="logo">CampusConnect</h5>
 
           <nav className="nav-links">
             <Link href="/">Home</Link>
             <Link href="/">Pages</Link>
             <Link href="/request">Request</Link>
             <Link href="/">Dashboard</Link>
+          
           </nav>
         </header>
 
-        {/* Page Content */}
-        {/* <main className="page-wrapper"> */}
-          {children}
-          {/* </main> */}
+        
+        {children}
       </body>
     </html>
   );
