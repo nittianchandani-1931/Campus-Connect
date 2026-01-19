db0a57c9de640fdaba37f5f89e4385859f8b804f
Chandani <nittianchandani@gmail.com>
2026-01-19 23:40:02 +0530

    some changes added

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,5 +1,7 @@
 import "./globals.css";
-import Link from "next/link";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/footer";
+
 
 export const metadata = {
   title: "CampusConnect",
@@ -10,27 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header className="navbar">
-          <Link href="/" className="logo" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-            <img src="/campus_logo.png" alt="CampusConnect Logo" style={{ height: "40px", width: "auto" }} />
-            <span style={{ color: "white", fontSize: "22px", fontWeight: "bold" }}>CampusConnect</span>
-          </Link>
-
-          <nav className="nav-links">
-            <Link href="/" className="nav-btn">
-              Home
-            </Link>
-            <Link href="/" className="nav-btn">
-              About
-            </Link>
-            <Link href="/" className="nav-btn">
-              Contact us
-            </Link>
-
-          </nav>
-        </header>
-
+        <Navbar />
         {children}
+        <Footer/>
       </body>
     </html>
   );
